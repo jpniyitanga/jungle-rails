9949b38574991f6619734aad4a88de0ce4d5cbff
Jean Pierre Niyitanga <jpniyitanga@gmail.com>
2023-07-02 15:42:01 -0400

    Fix money money format in My Cart page

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -2,5 +2,5 @@ Rails.configuration.stripe = {
   :publishable_key => ENV['STRIPE_PUBLISHABLE_KEY'],
   :secret_key      => ENV['STRIPE_SECRET_KEY']
 }
-
+puts Rails.configuration.stripe.inspect
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
