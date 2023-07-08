15aee5ba5b32c392053cce24216bf9f13590233e
Jean Pierre Niyitanga <jpniyitanga@gmail.com>
2023-07-08 11:59:24 -0400

    Logic to login and logout. Save session and destroy session

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,13 @@
 class SessionsController < ApplicationController
+  #before_action :authorize
+
   def new 
   end
 
   def create 
      user = User.find_by_email(params[:email])
     # If the user exists AND the password entered is correct.
-    if user && user.authenticate(params[:password_digest])
+    if user && user.authenticate(params[:password])
       # Save the user id inside the browser cookie. This is how we keep the user 
       # logged in when they navigate around our website.
       session[:user_id] = user.id
