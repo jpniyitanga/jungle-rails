db3b39626130947d83345a7de1e2c134c0ad98be
Jean Pierre Niyitanga <jpniyitanga@gmail.com>
2023-07-12 00:06:48 -0400

    fix signup and login forms. And add email validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,17 @@
 class User < ApplicationRecord
   has_secure_password
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+validates :email, presence: true,
+
+    uniqueness: { case_sensitive: false },
+
+    length: { maximum: 105 },
+
+    format: { with: VALID_EMAIL_REGEX }
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :password_digest, presence: true
 end
