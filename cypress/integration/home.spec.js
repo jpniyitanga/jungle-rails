406899630cb2f7c56ba240669b89208f0f33062b
Jean Pierre Niyitanga <jpniyitanga@gmail.com>
2023-07-21 21:28:34 -0400

    Implement testing homepage

---
+++ cypress/integration/home.spec.js
@@ -0,0 +1,13 @@
+describe("app homepage", () => {
+  it("visits the homepage", () => {
+    cy.visit("/");
+  });
+
+  it("There is products on the page", () => {
+    cy.get(".products article").should("be.visible");
+  });
+
+  it("There is 2 products on the page", () => {
+    cy.get(".products article").should("have.length", 2);
+  });
+});
